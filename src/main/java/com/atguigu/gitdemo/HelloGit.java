fed1f5e65b51246b2c907e038c59eca1d980aaf5
ychxing <1569835282@qq.com>
2021-09-25 14:38:56 +0800

    hotfix commit

--- src/main/java/com/atguigu/gitdemo/HelloGit.java
+++ src/main/java/com/atguigu/gitdemo/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("Hello Git!");
+        System.out.println("Hello hot_fix!");
     }
 }
