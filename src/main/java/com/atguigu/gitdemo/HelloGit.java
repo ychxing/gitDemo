a5cec0fcfad3fa845954db216aeb39987ec690ca
ychxing <1569835282@qq.com>
2021-09-25 14:29:34 +0800

    second commit

--- src/main/java/com/atguigu/gitdemo/HelloGit.java
+++ src/main/java/com/atguigu/gitdemo/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu.gitdemo;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("Hello Git!");
     }
 }
